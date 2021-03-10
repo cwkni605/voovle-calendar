ba57f0e4f623eae47bca1bf3b862198b3990a9d2
connor knight <cwkni605@gmail.com>
2021-03-09 22:14:20 -0700

    made site possible and added incomplete functions to add events
    I made site barely function with only a get. I added incomplete functions to add events, and I just need to add the final parameters to finish.

--- app.js
+++ app.js
@@ -24,35 +24,27 @@ oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
 // Create a new calender instance.
 const calendar = google.calendar({ version: 'v3', auth: oAuth2Client })
 
-// Create a new event start date instance for temp uses in our calendar.
-const eventStartTime = new Date();
-eventStartTime.setDate(eventStartTime.getDay() + 7);
 
-// Create a new event end date instance for temp uses in our calendar.
-const eventEndTime = new Date();
-eventEndTime.setDate(eventEndTime.getDay() + 7);
-eventEndTime.setMinutes(eventEndTime.getMinutes() + 45);
 // Create a template event for easy creation
-
 class NewCalendarEvent
 {
     /**  this is the event templete object
- * @param title -This is the title for the new event.
- * @param location -This is the location of the event.
- * @param description -This is the description of the event.
- * @param colorId -This is the id color for the event
- *  Color: Blue = colorId: 1 ||
- *  Color: Green = colorId: 2 ||
- *  Color: Purple = colorId: 3 ||
- *  Color: Red = colorId: 4 ||
- *  Color: Yellow = colorId: 5 ||
- *  Color: Orange = colorId: 6 ||
- *  Color: Turquoise = colorId: 7 ||
- *  Color: Gray = colorId: 8 ||
- *  Color: Bold Blue = colorId: 9 ||
- *  Color: Bold Green = colorId: 10 ||
- *  Color: bold red = colorId: 11 ||
-*/
+     * @param title -This is the title for the new event.
+     * @param location -This is the location of the event.
+     * @param description -This is the description of the event.
+     * @param colorId -This is the id color for the event
+     *  Color: Blue = colorId: 1 ||
+     *  Color: Green = colorId: 2 ||
+     *  Color: Purple = colorId: 3 ||
+     *  Color: Red = colorId: 4 ||
+     *  Color: Yellow = colorId: 5 ||
+     *  Color: Orange = colorId: 6 ||
+     *  Color: Turquoise = colorId: 7 ||
+     *  Color: Gray = colorId: 8 ||
+     *  Color: Bold Blue = colorId: 9 ||
+     *  Color: Bold Green = colorId: 10 ||
+     *  Color: bold red = colorId: 11 ||
+    */
     constructor(title, location, description, colorId)
     {
         this.summary = title,
@@ -60,50 +52,91 @@ class NewCalendarEvent
         this.description = description,
         this.colorId = colorId,
         this.start = {
-            dateTime: eventStartTime,
+            dateTime: eventStartTimeEval(0,0),
             timeZone: 'America/Denver',
         },
         this.end = {
-            dateTime: eventEndTime,
+            dateTime: eventEndTimeEval(0,0),
             timeZone: 'America/Denver',
         }
     }
 }
 
-//console.log(calendar);
+/**  this is the event templete objects endtime evaluating function for the start date
+ * @param day -This sets the day.
+ * @param hour -This sets the hour.
+*/
+function eventStartTimeEval(day,hours) {
+  const eventStartTime = new Date();
+  eventStartTime.setDate(eventStartTime.getDate());
+  eventStartTime.setHours(0, 0,0,0);
+  return eventStartTime;
+}
+
+/**  this is the event templete objects endtime evaluating function for the end date
+ * @param day -This sets the day.
+ * @param hour -This sets the hour.
+*/
+function eventEndTimeEval(day, hour) {
+  // Create a new event end date instance for temp uses in our calendar.
+  const eventEndTime = new Date();
+  eventEndTime.setDate(eventEndTime.getDate()+50);
+  eventEndTime.setHours(0, 0,0,0);
+  return eventEndTime;
+}
+
 //*
-// Check if we a busy and have an event on our calendar for the same time.
-calendar.freebusy.query(
-  {
-    resource: {
-      timeMin: eventStartTime,
-      timeMax: eventEndTime,
-      timeZone: 'America/Denver',
-      items: [{ id: 'primary' }],
+// Check if we are busy and add an event on our calendar at the same time.
+function addEventToCalendar(fileData){
+  let createdEvent = new NewCalendarEvent("testing", "This is date testing", "this is a location", 4);
+  calendar.freebusy.query(
+    {
+      resource: {
+        timeMin: createdEvent.start.dateTime,
+        timeMax: createdEvent.end.dateTime,
+        timeZone: 'America/Denver',
+        items: [{ id: 'primary' }],
+      },
     },
-  },
-  (err, res) => {
-    // Check for errors in our query and log them if they exist.
-    if (err) return console.error('Free Busy Query Error: ', err)
+    (err, res) => {
+      // Check for errors in our query and log them if they exist.
+      if (err) return console.error('Free Busy Query Error: ', err)
 
-    // Create an array of all events on our calendar during that time.
-    const eventArr = res.data.calendars.primary.busy
+      // Create an array of all events on our calendar during that time.
+      const eventArr = res.data.calendars.primary.busy
 
-    // Check if event array is empty which means we are not busy
-    if (eventArr.length === 0)
-      // If we are not busy create a new calendar event.
-      return calendar.events.insert(
-        { calendarId: 'primary', resource: new NewCalendarEvent("test", "testing testing one two three", "this is a location", 1) },
-        err => {
-          // Check for errors and log them if they exist.
-          if (err) return console.error('Error Creating Calender Event:', err)
-          // Else log that the event was created.
-          return console.log('Calendar event successfully created.')
-        }
-      );
-
-    // If event array is not empty log that we are busy.
-    return console.log(`Sorry I'm busy...`)
-  }
-);
+      // Check if event array is empty which means we are not busy
+      if (eventArr.length === 0){
+        // If we are not busy create a new calendar event.
+        return calendar.events.insert(
+          { calendarId: 'primary', resource: createdEvent }, err => {
+            // Check for errors and log them if they exist.
+            if (err) return console.error('Error Creating Calender Event:', err)
+            // Else log that the event was created.
+            addedEvent = true;
+            return console.log('Calendar event successfully created.')
+          }
+        );
+      } else {
+        // If event array is not empty log that we are busy.
+        return console.log(`Sorry I'm busy...`);
+      }
+    }
+  );
+}
 //*/
+
+app.get(/[\s\S]*/, function(req, res) {
+  let router = req.url;
+  if(router == "/favicon.ico") router = "/";
+  if (router.startsWith("/")) {
+    fs.readFile("index.html", "utf-8",(err, data)=>{
+      addEventToCalendar(data);
+      res.send(data);
+    });
+    return;
+  }
+});
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
